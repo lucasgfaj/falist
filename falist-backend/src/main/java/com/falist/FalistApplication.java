89ee33321cd9d6c5008f0ba9d1846a26b74845b4
lucasgfaj <lucasgfaj09@gmail.com>
2025-11-12 11:22:35 -0300

    Init a Project

---
+++ falist-backend/src/main/java/com/falist/FalistApplication.java
@@ -0,0 +1,13 @@
+package com.falist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalistApplication.class, args);
+	}
+
+}
